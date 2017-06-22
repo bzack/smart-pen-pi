d6e42f50a66352a5541ae51b0a36777515866265
Benjamin Zack <benjamin.zack@abbvie.com>
2017-06-22 16:38:07 -0400

    Added more endpoints to the flask app

--- smart-pen.py
+++ smart-pen.py
@@ -1,10 +1,15 @@
-from flask import Flask, g
+from flask import Flask, g, jsonify
 from random import randint, randrange
 import sqlite3
 from time import gmtime, time
 app = Flask(__name__)
 
-EVENT_TYPES=['PACKAGE_CREATED', 'MEASUREMENT', 'COUNTERFEIT_DETECTED', 'INJECTION', 'SPOILED']
+PACKAGE_CREATED = 'PACKAGE_CREATED'
+MEASUREMENT = 'MEASUREMENT'
+COUNTERFEIT_DETECTED = 'COUNTERFEIT_DETECTED'
+INJECTION = 'INJECTION'
+SPOILED = 'SPOILED'
+EVENT_TYPES=[PACKAGE_CREATED, MEASUREMENT, COUNTERFEIT_DETECTED, INJECTION, SPOILED]
 
 
 @app.route("/")
@@ -17,9 +22,9 @@ def create_fake(count):
     txt = ""
     base_utc = time() - (60*60*24 * randrange(30,90))
     for i in range(count):
-        id = "abbv%04d" % randint(0,9999)
+        id = "abbv%04d" % randint(2000,9999)
         for j in range(0,10):
-            event_type = 'MEASUREMENT'
+            event_type = MEASUREMENT
             time_utc = base_utc + 60*60*j
             lat = randrange(34.0,42.0)
             lng = randrange(-120.0,-76.0)
@@ -29,6 +34,30 @@ def create_fake(count):
         txt += id
     get_db().commit()
     return txt
+
+@app.route("/measurements/<string:id>", methods=["GET"])
+def get_measuremnts(id):
+    rslt = []
+    cur = get_db().cursor()
+    for row in cur.execute("select * from measurements where id = ?", (id,)):
+        rslt.append(dict_from_row(row))
+    return jsonify(rslt)
+
+@app.route("/ids", methods=["GET"])
+def get_ids():
+    rslt = []
+    cur = get_db().cursor()
+    for row in cur.execute("select distinct id from measurements"):
+        rslt.append(dict_from_row(row))
+    return jsonify(rslt)
+
+@app.route("/measuremnts", methods=['DELETE'])
+def delete_measurements():
+    get_db().execute("delete from measurements where id > 'abbv2000'")
+
+
+def dict_from_row(row):
+    return dict(zip(row.keys(), row))
 
 
 def record_measurement(id, time_utc, event_type, lat, lng, temp, light):
@@ -50,4 +79,5 @@ def get_db():
     """
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
+        g.sqlite_db.row_factory = sqlite3.Row
     return g.sqlite_db
