afce007670a2a4c652c44434614106ef73f76957
Benjamin Zack <benjamin.zack@abbvie.com>
2017-06-21 21:03:33 -0400

    Updated copy keys script

--- setup/set_keys.sh
+++ setup/set_keys.sh
@@ -5,7 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 AUTH_KEYS=${DIR}/authorized_keys.d
 AUTH_KEYS_FILE=${HOME}/.ssh/authorized_keys
 
-printf /dev/null > $AUTH_KEYS_FILE
+cat /dev/null > $AUTH_KEYS_FILE
+chmod 600 $AUTH_KEYS_FILE
 
 for f in ${AUTH_KEYS}/*; do
     cat $f >> $AUTH_KEYS_FILE
