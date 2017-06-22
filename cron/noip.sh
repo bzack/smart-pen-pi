b732f348042ff5e8d6bf703591d38bcaa10f9fed
Benjamin Zack <benjamin.zack@abbvie.com>
2017-06-22 17:16:32 -0400

    Changed dns to duckdns more reliable

--- cron/noip.sh
+++ cron/noip.sh
@@ -7,12 +7,13 @@ source ${DIR}/../config/env.sh
 echo $NOIP_USER
 echo $NOIP_PW
 
-PI_IP=`hostname -I`
+PI_IP=`hostname -I | sed -e 's/ //g'`
 
 echo $PI_IP
 
-URL="https://${NOIP_USER}:${NOIP_PW}@dynupdate.no-ip.com/nic/update?hostname=venusaur.ddns.net&myip=${PI_IP}"
+#URL="https://${NOIP_USER}:${NOIP_PW}@dynupdate.no-ip.com/nic/update?hostname=venusaur.ddns.net&myip=${PI_IP}"
+URL="https://www.duckdns.org/update?domains=smart-pen&token=${DUCK_TOKEN}&ip=${PI_IP}&verbose=true"
+#URL="https://www.duckdns.org/update?domains=venusaur&token=${DUCK_TOKEN}&ip=${PI_IP}&verbose=true"
 echo $URL
 
 curl -v "$URL"
-#curl -v https://www.duckdns.org/update?domains=venusaur&token=36396274-9076-4db8-9ebe-6c07a3483da6&ip=10.75.0.199&verbose=true
